141aa4c5060ad76b2aba65b9fdc3a7b0a8da419e
akshadagrawal <akshadagrawal.aa@gmail.com>
2021-06-23 17:47:05 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React from 'react';
+import FileUpload from './components/FileUpload';
 
 const App=()=> {
   return (
-    <div className="App">
-      Hi from akshad!
+    <div className="container mt-4">
+      <h4 className= "display-4 text-center mb-4">
+        <i className="fab fa-react"></i>React File upload
+      </h4>
+
+      <FileUpload />
+
     </div>
   );
 }
